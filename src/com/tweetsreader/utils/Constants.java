99f874ff2961e4b33c2cdfd2bc30c5f64aa61f59
Mohit <ag.mohit01@gmail.com>
2014-10-29 19:13:49 -0500

    Create README.md

--- src/com/tweetsreader/utils/Constants.java
+++ src/com/tweetsreader/utils/Constants.java
@@ -3,8 +3,8 @@ package com.tweetsreader.utils;
 
 public class Constants {
 	  public static String CALLBACKURL = "http://www.mohitagrawal.info";
-	  public static String consumerKey = "91MqJt6KdMrjsJDBp9e9A";
-	  public static String consumerSecret = "mOPKEzMCDktguuDtWGh8QhhzRz5sAkS4QZyXO0Tlzsw";
+	  public static String consumerKey = "CONSUMER_KEY";
+	  public static String consumerSecret = "CONSUMER_SECRET";
 	  public static String USER_KEY="user_key";
 	  public static String USER_SECRET = "user_secret";
 	  public static String TWEET_READER_PREF="Tweet_Reader_Prefs";	 
